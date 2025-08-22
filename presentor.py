3883854cd86b5c586e8f9ace4e304247fe567414
Evgeny <evgniy.t@gmail.com>
2025-08-22 18:28:39 +0300

    Stage 2
    blurring stuff

--- presentor.py
+++ presentor.py
@@ -12,17 +12,20 @@ def presenter_process(input_queue: Queue) -> None:
 
         frame, detections = item
 
+        blurred = cv2.GaussianBlur(frame, (21, 21), 0)
+        res = frame.copy()
         # Draw detections
         for (bbox, _) in detections:
             x, y, w, h = bbox
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            res[y:y + h, x: x + w] = blurred[y:y + h, x: x + w]
+            cv2.rectangle(res, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         # Draw timestamp
         timestamp = datetime.now().strftime("%H:%M:%S")
-        cv2.putText(frame, timestamp, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
+        cv2.putText(res, timestamp, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
 
         # Show frame
-        cv2.imshow("Presenter", frame)
+        cv2.imshow("Presenter", res)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
